78b96670620e3666c82d902408ae00332a38e91d
BKulzick <BryanKulczycki@gmail.com>
2015-04-28 08:56:36 -0400

    Changed to arrayList, ignore ----

--- DataMungingKata/src/main/application.java
+++ DataMungingKata/src/main/application.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -12,8 +13,11 @@ public class application {
 
 	public static void main(String[] args) {
 
+		String url = "http://codekata.com/data/04/weather.dat";
+		String url2 = "http://codekata.com/data/04/football.dat";
+
 		try {
-			application.getDataFileFromURL();
+			application.getDataFileFromURL(url2);
 		} catch (IOException e) {
 
 			e.printStackTrace();
@@ -21,45 +25,56 @@ public class application {
 
 	}
 
-	public static void getDataFileFromURL() throws IOException {
+	public static void getDataFileFromURL(String url) throws IOException {
 
-		URL URLtarget = new URL("http://codekata.com/data/04/weather.dat");
+		URL URLtarget = new URL(url);
 
 		InputStreamReader inputStreamReader = new InputStreamReader(
 				URLtarget.openStream());
 
 		BufferedReader bReader = new BufferedReader(inputStreamReader);
 
-		String tempString = bReader.readLine();
-
 		String zeroLine = bReader.readLine();
+
 		String firstLine;
+		String nextLine;
 		StringTokenizer st;
 
-		int[] checkThisDay = new int[3];
+		ArrayList checkThisRow = new ArrayList<Object>();
+		int spots = 0;
+
+		while (spots < 9) {
+			checkThisRow.add(0);
+			spots++;
+		}
+
+		System.out.println(checkThisRow.size());
 		int[] minimumChangeDay = { 0, 100, 0 };
-		
+
 		do {
-			firstLine = bReader.readLine();
+			nextLine = bReader.readLine();
 
-			if (firstLine.contains("mo"))
-				break;
+			if (!nextLine.contains("-----------")) {
 
-			st = new StringTokenizer(firstLine, " *");
-			for (int i = 0; i < 3; i++) {
-				checkThisDay[i] = Integer.parseInt(st.nextToken());
-			}
+				st = new StringTokenizer(nextLine, " *-");
 
-			if ((checkThisDay[1] - checkThisDay[2]) < (minimumChangeDay[1] - minimumChangeDay[2])) {
-				for (int i = 0; i < minimumChangeDay.length; i++) {
-					minimumChangeDay[i] = checkThisDay[i];
+				checkThisRow.set(0, st.nextToken()); // add Team #
+				checkThisRow.set(1, st.nextToken()); // add Team Name
+
+				for (int i = 2; i < 9; i++) {
+					checkThisRow.set(i, Integer.parseInt(st.nextToken()));
 				}
+				System.out.println(checkThisRow);
+				// if ((checkThisDay[1] - checkThisDay[2]) <
+				// (minimumChangeDay[1] - minimumChangeDay[2])) {
+				// for (int i = 0; i < minimumChangeDay.length; i++) {
+				// minimumChangeDay[i] = checkThisDay[i];
+				// }
 
-				
 			}
 
 		} while (bReader.ready());
 
-		System.out.println(Arrays.toString(minimumChangeDay));
+		System.out.println(checkThisRow);
 	}
 }
